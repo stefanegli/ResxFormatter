5c967996afa37111aa3adcb074ddd192b9389e09
Stefan Egli <stefan.egli@outlook.com>
2019-08-08 19:02:18 +0200

    Data Driven Tests instead of "normal" tests

--- ResxFormatterTests/FormattingTests.cs
+++ ResxFormatterTests/FormattingTests.cs
@@ -2,6 +2,8 @@
 {
     using NFluent;
     using ResxFormatter;
+    using ResxFormatterTests.TestFoundation;
+    using System.Collections.Generic;
     using System.IO;
     using System.Security.Cryptography;
     using System.Text;
@@ -9,135 +11,19 @@
 
     public class FormattingTests
     {
-        [Fact]
-        public void Additional_xml_comments_are_kept()
+        [Theory]
+        [ClassData(typeof(ResxTestData))]
+        public void Files_are_processed_correctly(string message, string fileName, string expectedHash)
         {
             // Arrange
             var formatter = new ResxFormatter(new FakeLog());
-            const string file = "_files\\AdditionalXmlComments.resx";
+            var file = $"_files\\{fileName}";
 
             // Act
             formatter.Run(file);
 
             // Assert
-            // TODO xml comments should retain their original position
-            Check.That(Sha256(file)).Equals("7A527237DE60E3C9308022A2EAC846427F5F55B021438EBD59CE3D480D3EDBCB");
-        }
-
-        [Fact]
-        public void Comment_is_removed_even_if_no_sorting_is_required()
-        {
-            // Arrange
-            var formatter = new ResxFormatter(new FakeLog());
-            const string file = "_files\\AlreadySorted.resx";
-
-            // Act
-            formatter.Run(file);
-
-            // Assert
-            Check.That(Sha256(file)).Equals("C63EB8803EA8CCEC3A4A3B81B07E12A074CCF9093EEA50DF935F6624388BD117");
-        }
-
-        [Fact]
-        public void Data_nodes_appear_after_meta_data_nodes()
-        {
-            // Arrange
-            var formatter = new ResxFormatter(new FakeLog());
-            const string file = "_files\\Mixed.resx";
-
-            // Act
-            formatter.Run(file);
-
-            // Assert
-            // TODO xml comments should retain their original position
-            Check.That(Sha256(file)).Equals("14D1520823028EB6505DFF8591CDF43B76128EA7C0F13A7EEC250EE66A49B445");
-        }
-
-        [Fact]
-        public void Entries_are_sorted_alphabetically()
-        {
-            // Arrange
-            var formatter = new ResxFormatter(new FakeLog());
-            const string file = "_files\\Resource1.resx";
-
-            // Act
-            formatter.Run(file);
-
-            // Assert
-            Check.That(Sha256(file)).Equals("6F32E0754A956B70FB7C09E29C47CAA38039A365FBE498A68DA73E698DA5D5DF");
-        }
-
-        [Fact]
-        public void File_remains_untouched_if_no_modification_is_necessary()
-        {
-            // Arrange
-            var formatter = new ResxFormatter(new FakeLog());
-            const string file = "_files\\NoModificationNeeded.resx";
-
-            // Act
-            formatter.Run(file);
-
-            // Assert
-            Check.That(Sha256(file)).Equals("10CFCF38BC20667B5163CBE310DD09AAB821A653DFCA04CC720F0A6F3349FD21");
-        }
-
-        [Fact]
-        public void Invalid_resx_files_are_not_touched()
-        {
-            // Arrange
-            var formatter = new ResxFormatter(new FakeLog());
-            const string file = "_files\\InvalidResx.resx";
-
-            // Act
-            formatter.Run(file);
-
-            // Assert
-            // TODO xml comments should retain their original position
-            Check.That(Sha256(file)).Equals("45DC5F4936DEA6FB3AC19465900F3237A2AA1A60C86D7113CADDD4B41C9805D0");
-        }
-
-        [Fact]
-        public void Meta_data_is_sorted_too()
-        {
-            // Arrange
-            var formatter = new ResxFormatter(new FakeLog());
-            const string file = "_files\\MetaData.resx";
-
-            // Act
-            formatter.Run(file);
-
-            // Assert
-            // TODO xml comments should retain their original position
-            Check.That(Sha256(file)).Equals("4B04A955A37723EC38CF5C5F45B279F95DBABBB1A0CC38A90C9B079D5BAFF7B2");
-        }
-
-        [Fact]
-        public void Plain_xml_files_are_not_touched()
-        {
-            // Arrange
-            var formatter = new ResxFormatter(new FakeLog());
-            const string file = "_files\\Plain.xml";
-
-            // Act
-            formatter.Run(file);
-
-            // Assert
-            // TODO xml comments should retain their original position
-            Check.That(Sha256(file)).Equals("9A56A89CF34541F785EE4F93A3BC754A6EB5632F4F6ABA3427A555BBD119827E");
-        }
-
-        [Fact]
-        public void Resx_comment_nodes_are_kept()
-        {
-            // Arrange
-            var formatter = new ResxFormatter(new FakeLog());
-            const string file = "_files\\WithResxComments.resx";
-
-            // Act
-            formatter.Run(file);
-
-            // Assert
-            Check.That(Sha256(file)).Equals("FA02F7FCCC956A3424EF4F30CF9ED2D097C0B74EBEFC4ABE27FCE56D0FF3B910");
+            Check.WithCustomMessage(message).That(Sha256(file)).Equals(expectedHash);
         }
 
         private string Sha256(string path)
@@ -153,6 +39,23 @@
                 }
 
                 return result.ToString();
+            }
+        }
+
+        internal class ResxTestData : TheoryDataBase<string, string, string>
+        {
+            public override IEnumerable<(string, string, string)> Create()
+            {
+                yield return ("Additional xml comments are kept.", "AdditionalXmlComments.resx", "7A527237DE60E3C9308022A2EAC846427F5F55B021438EBD59CE3D480D3EDBCB");
+                yield return ("Comment is removed even if no sorting is required.", "AlreadySorted.resx", "C63EB8803EA8CCEC3A4A3B81B07E12A074CCF9093EEA50DF935F6624388BD117");
+                yield return ("Data nodes appear after meta data nodes.", "Mixed.resx", "14D1520823028EB6505DFF8591CDF43B76128EA7C0F13A7EEC250EE66A49B445");
+                yield return ("Entries are sorted alphabetically.", "Resource1.resx", "6F32E0754A956B70FB7C09E29C47CAA38039A365FBE498A68DA73E698DA5D5DF");
+                yield return ("File remains untouched if no modification is necessary.", "NoModificationNeeded.resx", "10CFCF38BC20667B5163CBE310DD09AAB821A653DFCA04CC720F0A6F3349FD21");
+                // TODO xml comments should retain their original position
+                yield return ("Invalid resx files are not touched.", "InvalidResx.resx", "45DC5F4936DEA6FB3AC19465900F3237A2AA1A60C86D7113CADDD4B41C9805D0");
+                yield return ("Meta data is sorted too.", "MetaData.resx", "4B04A955A37723EC38CF5C5F45B279F95DBABBB1A0CC38A90C9B079D5BAFF7B2");
+                yield return ("Plain xml files are not touched.", "Plain.xml", "9A56A89CF34541F785EE4F93A3BC754A6EB5632F4F6ABA3427A555BBD119827E");
+                yield return ("Comment nodes are kept.", "WithResxComments.resx", "FA02F7FCCC956A3424EF4F30CF9ED2D097C0B74EBEFC4ABE27FCE56D0FF3B910");
             }
         }
     }
---
+++ ResxFormatterTests/TestFoundation/TheoryDataBase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResxFormatterTests.TestFoundation
+{
+    internal abstract class TheoryDataBase : IEnumerable<object[]>
+    {
+        public abstract IEnumerator<object[]> GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+
+    internal abstract class TheoryDataBase<T> : TheoryDataBase
+    {
+        public abstract IEnumerable<T> Create();
+
+        public override IEnumerator<object[]> GetEnumerator()
+        {
+            return this.Create().Select(o => new object[] { o }).GetEnumerator();
+        }
+    }
+
+    internal abstract class TheoryDataBase<T1, T2> : TheoryDataBase
+    {
+        public abstract IEnumerable<(T1, T2)> Create();
+
+        public override IEnumerator<object[]> GetEnumerator()
+        {
+            return this.Create().Select(o => new object[] { o.Item1, o.Item2 }).GetEnumerator();
+        }
+    }
+
+    internal abstract class TheoryDataBase<T1, T2, T3> : TheoryDataBase
+    {
+        public abstract IEnumerable<(T1, T2, T3)> Create();
+
+        public override IEnumerator<object[]> GetEnumerator()
+        {
+            return this.Create().Select(o => new object[] { o.Item1, o.Item2, o.Item3 }).GetEnumerator();
+        }
+    }
+}
