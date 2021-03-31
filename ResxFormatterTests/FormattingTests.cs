8651ffe6c9c17437c19263055caa8be4fe01a468
Stefan Egli <stefan.egli@outlook.com>
2021-03-31 18:14:55 +0200

    Removed Hash checks

--- ResxFormatterTests/FormattingTests.cs
+++ ResxFormatterTests/FormattingTests.cs
@@ -5,8 +5,6 @@
     using ResxFormatterTests.TestFoundation;
     using System.Collections.Generic;
     using System.IO;
-    using System.Security.Cryptography;
-    using System.Text;
     using System.Threading;
     using Xunit;
 
@@ -14,10 +12,11 @@
     {
         [Theory]
         [ClassData(typeof(ResxTestData))]
-        public void Files_are_processed_correctly(ISettings settings, string message, string fileName, string culture, string expectedHash)
+        public void Files_are_processed_correctly(ISettings settings, string message, string fileName, string culture)
         {
             // Arrange
-            var tempFileName = $"_files\\{Path.GetFileNameWithoutExtension(fileName)}-actual.resx";
+            string baseFileName = Path.GetFileNameWithoutExtension(fileName);
+            var tempFileName = $"_files\\{baseFileName}-actual.resx";
             var expectedFileName = $"_files\\{Path.GetFileNameWithoutExtension(fileName)}-expected.resx";
 
             var formatter = new ResxFormatter(settings, new FakeLog());
@@ -31,32 +30,14 @@
             formatter.Run(tempFileName);
 
             // Assert
-            Check.WithCustomMessage(message + $" Result File: {tempFileName}").That(Sha256(tempFileName)).Equals(expectedHash);
-
             var actualLines = File.ReadAllLines(tempFileName);
             var expectedLines = File.ReadAllLines(expectedFileName);
             Check.That(actualLines).Equals(expectedLines);
         }
 
-        private string Sha256(string path)
+        internal class ResxTestData : TheoryDataBase<ISettings, string, string, string>
         {
-            using (var stream = File.OpenRead(path))
-            using (var sha = new SHA256Managed())
-            {
-                var result = new StringBuilder();
-                byte[] hash = sha.ComputeHash(stream);
-                foreach (byte hashByte in hash)
-                {
-                    result.Append(hashByte.ToString("X2"));
-                }
-
-                return result.ToString();
-            }
-        }
-
-        internal class ResxTestData : TheoryDataBase<ISettings, string, string, string, string>
-        {
-            public override IEnumerable<(ISettings, string, string, string, string)> Create()
+            public override IEnumerable<(ISettings, string, string, string)> Create()
             {
                 var @default = new FakeSettings
                 {
@@ -64,17 +45,17 @@
                     RemoveDocumentationComment = true
                 };
 
-                yield return (@default, "Culture", "InvariantCulture.resx", "et", "2B44847918C2EE7F2D7803BA130F6B5E13AEA28C32758A3E0BA2BEF7F51DF5A1");
-                yield return (@default, "Additional xml comments are kept.", "AdditionalXmlComments.resx", null, "7A527237DE60E3C9308022A2EAC846427F5F55B021438EBD59CE3D480D3EDBCB");
-                yield return (@default, "Comment is removed even if no sorting is required.", "AlreadySorted.resx", null, "C63EB8803EA8CCEC3A4A3B81B07E12A074CCF9093EEA50DF935F6624388BD117");
-                yield return (@default, "Data nodes appear after meta data nodes.", "Mixed.resx", null, "14D1520823028EB6505DFF8591CDF43B76128EA7C0F13A7EEC250EE66A49B445");
-                yield return (@default, "Entries are sorted alphabetically.", "Sort.resx", null, "6F32E0754A956B70FB7C09E29C47CAA38039A365FBE498A68DA73E698DA5D5DF");
-                yield return (@default, "File remains untouched if no modification is necessary.", "NoModificationNeeded.resx", null, "10CFCF38BC20667B5163CBE310DD09AAB821A653DFCA04CC720F0A6F3349FD21");
+                yield return (@default, "Culture", "InvariantCulture.resx", "et");
+                yield return (@default, "Additional xml comments are kept.", "AdditionalXmlComments.resx", null);
+                yield return (@default, "Comment is removed even if no sorting is required.", "AlreadySorted.resx", null);
+                yield return (@default, "Data nodes appear after meta data nodes.", "Mixed.resx", null);
+                yield return (@default, "Entries are sorted alphabetically.", "Sort.resx", null);
+                yield return (@default, "File remains untouched if no modification is necessary.", "NoModificationNeeded.resx", null);
                 // TODO xml comments should retain their original position
-                yield return (@default, "Invalid resx files are not touched.", "InvalidResx.resx", null, "45DC5F4936DEA6FB3AC19465900F3237A2AA1A60C86D7113CADDD4B41C9805D0");
-                yield return (@default, "Meta data is sorted too.", "MetaData.resx", null, "4B04A955A37723EC38CF5C5F45B279F95DBABBB1A0CC38A90C9B079D5BAFF7B2");
-                yield return (@default, "Plain xml files are not touched.", "Plain.xml", null, "9A56A89CF34541F785EE4F93A3BC754A6EB5632F4F6ABA3427A555BBD119827E");
-                yield return (@default, "Comment nodes are kept.", "WithResxComments.resx", null, "FA02F7FCCC956A3424EF4F30CF9ED2D097C0B74EBEFC4ABE27FCE56D0FF3B910");
+                yield return (@default, "Invalid resx files are not touched.", "InvalidResx.resx", null);
+                yield return (@default, "Meta data is sorted too.", "MetaData.resx", null);
+                yield return (@default, "Plain xml files are not touched.", "Plain.xml", null);
+                yield return (@default, "Comment nodes are kept.", "WithResxComments.resx", null);
 
                 var noSort = new FakeSettings
                 {
@@ -82,7 +63,7 @@
                     RemoveDocumentationComment = true
                 };
 
-                yield return (noSort, "Entries are only sorted if 'sort' setting is active.", "DoNotSort.resx", null, "2C4EFA8D11C3B6AE7308FFB182B1794E6D61106B5771B120B06C559C0D1E52DC");
+                yield return (noSort, "Entries are only sorted if 'sort' setting is active.", "DoNotSort.resx", null);
 
                 var keepComment = new FakeSettings
                 {
@@ -90,7 +71,7 @@
                     RemoveDocumentationComment = false
                 };
 
-                yield return (keepComment, "Documentation is only removed if 'doc' setting is active.", "KeepComments.resx", null, "E47BF6D02610D33256E4A0E913669CF4A29BB9ED0FEB8DC43628277E960092B8");
+                yield return (keepComment, "Documentation is only removed if 'doc' setting is active.", "KeepComments.resx", null);
 
                 var doNothing = new FakeSettings
                 {
@@ -98,7 +79,7 @@
                     RemoveDocumentationComment = false
                 };
 
-                yield return (doNothing, "Documentation is only removed if 'doc' setting is active.", "DoNothing.resx", null, "D9643216D1B2B631BF12C0A3742C570AD460DB634636FB58FD9B98D8565A61F4");
+                yield return (doNothing, "Documentation is only removed if 'doc' setting is active.", "DoNothing.resx", null);
             }
 
             private class FakeSettings : ISettings
