489acb638d66cf35a724df5149176339660dd5f3
Stefan Egli <stefan.egli@outlook.com>
2021-03-31 18:10:16 +0200

    Improved Unit-Tests (compare files)

--- ResxFormatterTests/FormattingTests.cs
+++ ResxFormatterTests/FormattingTests.cs
@@ -3,7 +3,6 @@
     using NFluent;
     using ResxFormatter;
     using ResxFormatterTests.TestFoundation;
-    using System;
     using System.Collections.Generic;
     using System.IO;
     using System.Security.Cryptography;
@@ -18,7 +17,8 @@
         public void Files_are_processed_correctly(ISettings settings, string message, string fileName, string culture, string expectedHash)
         {
             // Arrange
-            var tempFileName = $"_files\\{Path.GetFileNameWithoutExtension(fileName)}-{Guid.NewGuid()}.resx";
+            var tempFileName = $"_files\\{Path.GetFileNameWithoutExtension(fileName)}-actual.resx";
+            var expectedFileName = $"_files\\{Path.GetFileNameWithoutExtension(fileName)}-expected.resx";
 
             var formatter = new ResxFormatter(settings, new FakeLog());
             var file = $"_files\\{fileName}";
@@ -32,6 +32,10 @@
 
             // Assert
             Check.WithCustomMessage(message + $" Result File: {tempFileName}").That(Sha256(tempFileName)).Equals(expectedHash);
+
+            var actualLines = File.ReadAllLines(tempFileName);
+            var expectedLines = File.ReadAllLines(expectedFileName);
+            Check.That(actualLines).Equals(expectedLines);
         }
 
         private string Sha256(string path)
@@ -64,7 +68,7 @@
                 yield return (@default, "Additional xml comments are kept.", "AdditionalXmlComments.resx", null, "7A527237DE60E3C9308022A2EAC846427F5F55B021438EBD59CE3D480D3EDBCB");
                 yield return (@default, "Comment is removed even if no sorting is required.", "AlreadySorted.resx", null, "C63EB8803EA8CCEC3A4A3B81B07E12A074CCF9093EEA50DF935F6624388BD117");
                 yield return (@default, "Data nodes appear after meta data nodes.", "Mixed.resx", null, "14D1520823028EB6505DFF8591CDF43B76128EA7C0F13A7EEC250EE66A49B445");
-                yield return (@default, "Entries are sorted alphabetically.", "Resource1.resx", null, "6F32E0754A956B70FB7C09E29C47CAA38039A365FBE498A68DA73E698DA5D5DF");
+                yield return (@default, "Entries are sorted alphabetically.", "Sort.resx", null, "6F32E0754A956B70FB7C09E29C47CAA38039A365FBE498A68DA73E698DA5D5DF");
                 yield return (@default, "File remains untouched if no modification is necessary.", "NoModificationNeeded.resx", null, "10CFCF38BC20667B5163CBE310DD09AAB821A653DFCA04CC720F0A6F3349FD21");
                 // TODO xml comments should retain their original position
                 yield return (@default, "Invalid resx files are not touched.", "InvalidResx.resx", null, "45DC5F4936DEA6FB3AC19465900F3237A2AA1A60C86D7113CADDD4B41C9805D0");
@@ -78,7 +82,7 @@
                     RemoveDocumentationComment = true
                 };
 
-                yield return (noSort, "Entries are only sorted if 'sort' setting is active.", "Resource1.resx", null, "2C4EFA8D11C3B6AE7308FFB182B1794E6D61106B5771B120B06C559C0D1E52DC");
+                yield return (noSort, "Entries are only sorted if 'sort' setting is active.", "DoNotSort.resx", null, "2C4EFA8D11C3B6AE7308FFB182B1794E6D61106B5771B120B06C559C0D1E52DC");
 
                 var keepComment = new FakeSettings
                 {
@@ -86,7 +90,7 @@
                     RemoveDocumentationComment = false
                 };
 
-                yield return (keepComment, "Documentation is only removed if 'doc' setting is active.", "Resource1.resx", null, "E47BF6D02610D33256E4A0E913669CF4A29BB9ED0FEB8DC43628277E960092B8");
+                yield return (keepComment, "Documentation is only removed if 'doc' setting is active.", "KeepComments.resx", null, "E47BF6D02610D33256E4A0E913669CF4A29BB9ED0FEB8DC43628277E960092B8");
 
                 var doNothing = new FakeSettings
                 {
@@ -94,13 +98,13 @@
                     RemoveDocumentationComment = false
                 };
 
-                yield return (doNothing, "Documentation is only removed if 'doc' setting is active.", "Resource1.resx", null, "D9643216D1B2B631BF12C0A3742C570AD460DB634636FB58FD9B98D8565A61F4");
+                yield return (doNothing, "Documentation is only removed if 'doc' setting is active.", "DoNothing.resx", null, "D9643216D1B2B631BF12C0A3742C570AD460DB634636FB58FD9B98D8565A61F4");
             }
 
             private class FakeSettings : ISettings
             {
-                public bool FixResxWriter => throw new NotImplementedException();
-                public ReloadMode ReloadFile => throw new NotImplementedException();
+                public bool FixResxWriter => false;
+                public ReloadMode ReloadFile => ReloadMode.Off;
                 public bool RemoveDocumentationComment { get; set; }
                 public bool SortEntries { get; set; }
             }
