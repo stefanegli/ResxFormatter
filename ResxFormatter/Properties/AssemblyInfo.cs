055d9da7aff10de1cae5a23d237dc1dc50b45142
Stefan Egli <stefan.egli@outlook.com>
2022-03-22 21:39:20 +0100

    BuildFix: Culture cannot be set or else the tests fail

--- ResxFormatter/Properties/AssemblyInfo.cs
+++ ResxFormatter/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct(Vsix.Name)]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture(Vsix.Language)]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
