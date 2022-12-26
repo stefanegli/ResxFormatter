738484ff9586f97dd760e431b5b88235c6eab0fd
Stefan Egli <stefan.egli@outlook.com>
2022-12-26 18:27:01 +0100

    Top level menu for commands

--- ResxFormatter/Commands/ResxFormatterPackage.cs
+++ ResxFormatter/Commands/ResxFormatterPackage.cs
@@ -15,6 +15,9 @@ namespace ResxFormatter.Commands
         public const string guidResxFormatterPackageString = "40d1f52e-e828-4cca-8279-df4ccd348f09";
         public static Guid guidResxFormatterPackage = new Guid(guidResxFormatterPackageString);
 
+        public const string guidResxFormatterPackageTopMenuCmdSetString = "056b1778-6320-485b-8e99-ed6f2e1cbf30";
+        public static Guid guidResxFormatterPackageTopMenuCmdSet = new Guid(guidResxFormatterPackageTopMenuCmdSetString);
+
         public const string guidResxFormatterPackageCmdSetString = "c7e059c9-69b5-443d-88f4-930c6a3975ca";
         public static Guid guidResxFormatterPackageCmdSet = new Guid(guidResxFormatterPackageCmdSetString);
     }
@@ -23,7 +26,9 @@ namespace ResxFormatter.Commands
     /// </summary>
     internal sealed partial class PackageIds
     {
-        public const int ResxFormatterCommands = 0x1020;
+        public const int ResxFormatterTopMenu = 0x1020;
+        public const int ResxFormatterTopMenu1 = 0x1020;
+        public const int ResxFormatterCommands = 0x1021;
         public const int FormatAllCommandId = 0x0100;
     }
 }
